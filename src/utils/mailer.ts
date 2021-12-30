015736f305c853cc1924be5a3314dc3fe27aa102
Tom Nagle <tomanagle@gmail.com>
2021-12-30 12:04:26 +1000

    mailer

---
+++ src/utils/mailer.ts
@@ -0,0 +1,36 @@
+import nodemailer, { SendMailOptions } from "nodemailer";
+import config from "config";
+import log from "./logger";
+
+// async function createTestCreds() {
+//   const creds = await nodemailer.createTestAccount();
+//   console.log({ creds });
+// }
+
+// createTestCreds();
+
+const smtp = config.get<{
+  user: string;
+  pass: string;
+  host: string;
+  port: number;
+  secure: boolean;
+}>("smtp");
+
+const transporter = nodemailer.createTransport({
+  ...smtp,
+  auth: { user: smtp.user, pass: smtp.pass },
+});
+
+async function sendEmail(payload: SendMailOptions) {
+  transporter.sendMail(payload, (err, info) => {
+    if (err) {
+      log.error(err, "Error sending email");
+      return;
+    }
+
+    log.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+  });
+}
+
+export default sendEmail;
